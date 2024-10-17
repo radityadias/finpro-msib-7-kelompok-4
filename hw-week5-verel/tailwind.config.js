6b6641e77dae16783b24b5edb1e67bde65271648
unknown <vereladitya07@gmail.com>
2024-10-17 22:59:39 +0800

    menambahkan file hw verel

---
+++ hw-week5-verel/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "profile.html", "course.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
